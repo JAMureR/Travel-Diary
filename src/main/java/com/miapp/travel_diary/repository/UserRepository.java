472385cd78c02ba6d925ed1bd4948d9bde7dc3fb
JAMureR <jmuresat@gmail.com>
2025-07-14 15:51:31 +0100

    creacion modelo User y UserRepository

---
+++ src/main/java/com/miapp/travel_diary/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.miapp.travel_diary.repository;
+
+import com.miapp.travel_diary.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
